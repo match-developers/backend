b77da6a837ad7ec8bf5d57f773829eeb3e804f13
Felipe López <fglf18@gmail.com>
2024-03-19 17:02:03 -0300

    Add gallery to club and sportground

--- clubs/models.py
+++ clubs/models.py
@@ -1,6 +1,27 @@
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+
+from model_utils.models import TimeStampedModel
+
+from accounts.models import Account
+from newsfeed.models import Comment, Like
 
 
 class Club(models.Model):
     name = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+
+class ClubGallery(TimeStampedModel):
+    image = models.ImageField(upload_to="clubs/gallery/")
+    description = models.TextField()
+    club = models.ForeignKey(Club, related_name="gallery", on_delete=models.CASCADE)
+
+
+class ClubTransferInvitePost(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    comments = GenericRelation(Comment)
+    likes = GenericRelation(Like)
--- sportsgrounds/models.py
+++ sportsgrounds/models.py
@@ -1,6 +1,17 @@
 from django.contrib.gis.db import models
 
+from model_utils.models import TimeStampedModel
+
 
 class SportGround(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
+    description = models.TextField(blank=True, null=True)
+
+
+class SportGroundGallery(TimeStampedModel):
+    image = models.ImageField(upload_to="sportsgrounds/gallery/")
+    description = models.TextField()
+    sportground = models.ForeignKey(
+        SportGround, related_name="gallery", on_delete=models.CASCADE
+    )
