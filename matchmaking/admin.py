c6ef4005362d9f175060d7faa9621e16b9ab2eff
Felipe López <fglf18@gmail.com>
2024-03-20 23:13:02 -0300

    Add admin site changes for some models

--- matchmaking/admin.py
+++ matchmaking/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import FriendlyClubMatch, Goal, Match, MatchParticipant, MatchScore
+from .models import (
+    FriendlyClubMatch,
+    Goal,
+    Match,
+    MatchParticipant,
+    MatchPost,
+    MatchScore,
+)
 
 admin.site.register(Match)
 admin.site.register(MatchParticipant)
 admin.site.register(MatchScore)
 admin.site.register(Goal)
 admin.site.register(FriendlyClubMatch)
+admin.site.register(MatchPost)
--- sportsgrounds/admin.py
+++ sportsgrounds/admin.py
@@ -2,5 +2,14 @@ from django.contrib.gis import admin
 
 from .models import SportGround, SportGroundGallery
 
-admin.site.register(SportGround, admin.OSMGeoAdmin)
-admin.site.register(SportGroundGallery)
+
+class SportGroundGalleryInline(admin.StackedInline):
+    model = SportGroundGallery
+    extra = 1  # Number of extra forms to display
+
+
+class SportGroundAdmin(admin.OSMGeoAdmin):
+    inlines = [SportGroundGalleryInline]
+
+
+admin.site.register(SportGround, SportGroundAdmin)
