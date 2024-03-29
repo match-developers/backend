a52e5562a7325c4086f602b8e0e2f2e831eee0f4
Felipe López <fglf18@gmail.com>
2024-03-29 12:39:41 -0300

    Adjust admin site for CustomPost

--- newsfeed/admin.py
+++ newsfeed/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
-from .models import Comment, CustomPost
+from .models import (
+    Comment,
+    CustomPost,
+    ImageAttachment,
+    TextAttachment,
+    VideoAttachment,
+)
 
-admin.site.register(CustomPost)
+
+class TextAttachmentInline(GenericTabularInline):
+    model = TextAttachment
+
+
+class VideoAttachmentInline(GenericTabularInline):
+    model = VideoAttachment
+
+
+class ImageAttachmentInline(GenericTabularInline):
+    model = ImageAttachment
+
+
+class CustomPostAdmin(admin.ModelAdmin):
+    inlines = [TextAttachmentInline, VideoAttachmentInline, ImageAttachmentInline]
+    list_display = ("id", "title")
+
+
+admin.site.register(CustomPost, CustomPostAdmin)
 admin.site.register(Comment)
